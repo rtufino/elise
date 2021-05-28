e0cfd43750a8c567c863861a45e0888ae4f2c4dd
Anthony Cabezas <anthony.cabezas@infinit-plus.com>
2021-05-27 19:49:08 -0500

    all models added

--- investigacion/models.py
+++ investigacion/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from estudiantes.models import Carrera, Alumno, Periodo
-from formulario.models import Encuesta
+from formulario.models import Encuesta, Categoria
 # Create your models here.
 
 class Evaluacion(models.Model):
@@ -22,20 +22,21 @@ class Asignacion(models.Model):
     alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
     encuesta = models.ForeignKey(Encuesta, on_delete=models.CASCADE)
     tipo = models.CharField(max_length=30)
-    class Meta:
-        verbose_name = 'Asignacion'
-    def __str__(self):
-        return self.alumno
 
 class Termino(models.Model):
     formula = models.ForeignKey(Formula, on_delete=models.CASCADE)
     signo = models.CharField(max_length=5)
     valor = models.DecimalField(decimal_places=10,max_digits=10)
+
 class Rendimiento(models.Model):
     formula = models.ForeignKey(Formula, on_delete=models.CASCADE)
     rendimiento_satisfactorio= models.DecimalField(decimal_places=10)
     rendimiento_riesgoso = models.DecimalField(decimal_places=10,max_digits=10)
     afinidad =models.BooleanField()
+
+class formula_categoria(models.Model):
+    formula = models.ForeignKey(Formula, on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
 
 class Parametro(models.Model):
     clave = models.CharField(max_length=100)
